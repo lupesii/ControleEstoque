790ba4e1efa9efe36818b7d345f8816d5549c17f
Lucas Silva e Pereira <lucaspereira.dev@proton.me>
2023-10-04 21:45:15 -0300

    Desenvolvido a tela Splash

---
+++ ControleEstoque/Splash.Designer.cs
@@ -0,0 +1,121 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace ControleEstoque
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblEmpresa = new System.Windows.Forms.Label();
+            this.lblEstoque = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 50;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(10, 154);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(176, 29);
+            this.progressBar.TabIndex = 3;
+            this.progressBar.UseWaitCursor = true;
+            // 
+            // lblEmpresa
+            // 
+            this.lblEmpresa.AutoSize = true;
+            this.lblEmpresa.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.lblEmpresa.Location = new System.Drawing.Point(31, 94);
+            this.lblEmpresa.Name = "lblEmpresa";
+            this.lblEmpresa.Size = new System.Drawing.Size(134, 19);
+            this.lblEmpresa.TabIndex = 0;
+            this.lblEmpresa.Text = "Lucas\'s Enterprise";
+            this.lblEmpresa.UseWaitCursor = true;
+            // 
+            // lblEstoque
+            // 
+            this.lblEstoque.AutoSize = true;
+            this.lblEstoque.Font = new System.Drawing.Font("Tahoma", 9.75F);
+            this.lblEstoque.Location = new System.Drawing.Point(37, 111);
+            this.lblEstoque.Name = "lblEstoque";
+            this.lblEstoque.Size = new System.Drawing.Size(122, 16);
+            this.lblEstoque.TabIndex = 1;
+            this.lblEstoque.Text = "Controle de Estoque";
+            this.lblEstoque.UseWaitCursor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ControleEstoque.Properties.Resources.Estoque;
+            this.pictureBox1.Location = new System.Drawing.Point(58, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(81, 79);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(196, 194);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.lblEstoque);
+            this.Controls.Add(this.lblEmpresa);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Carregando...";
+            this.UseWaitCursor = true;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer;
+        private ProgressBar progressBar;
+        private Label lblEmpresa;
+        private Label lblEstoque;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ ControleEstoque/Splash.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace ControleEstoque
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (progressBar.Value < 100)
+            {
+                progressBar.Value = progressBar.Value + 1;
+            }
+            else
+            {
+                timer.Enabled = false;
+                this.Visible = false;
+
+                //frmLogin telaLogin = new frmLogin();
+                //telaLogin.Show();
+            }
+        }
+    }
+}
